28a00b5b7c14355ee2aa208f3a30e5218dd220f4
rogerio.mattos7 <rogeriomattos38@outlook.com>
2020-05-20 13:38:48 -0300

    init solution

---
+++ src/webparts/toDoListApp/components/IToDoListAppProps.ts
@@ -0,0 +1,3 @@
+export interface IToDoListAppProps {
+  description: string;
+}
---
+++ src/webparts/toDoListApp/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IToDoListAppWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ToDoListAppWebPartStrings' {
+  const strings: IToDoListAppWebPartStrings;
+  export = strings;
+}
